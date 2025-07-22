ca4a6366e5f5246bc94dcf1dd7b350f21e97c597
Masud Rana <masudrana730@gmail.com>
2025-07-22 17:07:40 +0600

    updated swiper slider

--- swiper.js
+++ swiper.js
@@ -1,13 +1,31 @@
-var swiper = new Swiper(".mySwiper", 
-    {
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 30,
+  slidesPerGroup: 3, // This stays fixed
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".custom-button-prev",
+    prevEl: ".custom-button-next",
+  },
+  breakpoints: {
+    // when window width is >= 992px (desktop)
+    992: {
       slidesPerView: 3,
-      spaceBetween: 30,
-      pagination: {
-        el: ".swiper-pagination",
-        type: "progressbar",
-      },
-       navigation: {
-        nextEl: ".custom-button-next",
-        prevEl: ".custom-button-prev",
-      },
-    });
+      slidesPerGroup: 3,
+    },
+    // when window width is >= 768px (tablet)
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    // when window width is < 768px (mobile)
+    0: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+    },
+  },
+});
