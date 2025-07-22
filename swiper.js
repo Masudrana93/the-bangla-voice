6996bfe12fc338a3fb6164271305aca27d4e3bb2
Masud Rana <masudrana730@gmail.com>
2025-07-22 15:05:21 +0600

    slider updated

--- swiper.js
+++ swiper.js
@@ -6,4 +6,8 @@ var swiper = new Swiper(".mySwiper",
         el: ".swiper-pagination",
         type: "progressbar",
       },
+       navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
     });
